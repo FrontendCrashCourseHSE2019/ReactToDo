0b54f1b663652c50109f6be94b047e43ac27db0e
Semyon Danilov <samvimes@yandex.ru>
2019-10-24 18:41:53 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {InputComponent} from "./InputComponent";
 import {Nav} from "./components/Nav";
 import MainComponent from "./MainComponent";
 
 const App: React.FC = () => {
-  return (
-      <div className="App">
-        <Nav />
-        <MainComponent/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Nav/>
+            <MainComponent/>
+        </div>
+    );
 };
 
 export default App;
